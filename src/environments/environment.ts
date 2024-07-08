04aededa8d3d5ef5ee138f11f9b9c43f42ec1597
Harshit Gupta <kohthecodemaster@gmail.com>
2024-07-08 17:35:00 +0530

    #6 - Phase 1 - Skeleton Design - Adding Dependencies
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBxwbunXCibucF2RA9AbMzPpU0RAU3XXSk",
+        authDomain: "g-shop-89c63.firebaseapp.com",
+        projectId: "g-shop-89c63",
+        storageBucket: "g-shop-89c63.appspot.com",
+        messagingSenderId: "301226694100",
+        appId: "1:301226694100:web:79c0194603ab573dd67241",
+        measurementId: "G-9T7MK04617"
+    }
+};
