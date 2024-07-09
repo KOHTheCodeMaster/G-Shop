edeaeafe7ea16f8b035f270dde7c585fb4383d1c
Harshit Gupta <kohthecodemaster@gmail.com>
2024-07-09 09:59:47 +0530

    #8 - Phase 1 - Skeleton Design - Adding Firebase Dependencies
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,20 @@ import {ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getMessaging, provideMessaging } from '@angular/fire/messaging';
-import { getStorage, provideStorage } from '@angular/fire/storage';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {getMessaging, provideMessaging} from '@angular/fire/messaging';
+import {getStorage, provideStorage} from '@angular/fire/storage';
+import {environment} from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
     // providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-    providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"g-shop-89c63","appId":"1:301226694100:web:79c0194603ab573dd67241","storageBucket":"g-shop-89c63.appspot.com","locationId":"us-central","apiKey":"AIzaSyBxwbunXCibucF2RA9AbMzPpU0RAU3XXSk","authDomain":"g-shop-89c63.firebaseapp.com","messagingSenderId":"301226694100","measurementId":"G-9T7MK04617"})), provideAuth(() => getAuth()), provideFirebaseApp(() => initializeApp({"projectId":"g-shop-89c63","appId":"1:301226694100:web:79c0194603ab573dd67241","storageBucket":"g-shop-89c63.appspot.com","locationId":"us-central","apiKey":"AIzaSyBxwbunXCibucF2RA9AbMzPpU0RAU3XXSk","authDomain":"g-shop-89c63.firebaseapp.com","messagingSenderId":"301226694100","measurementId":"G-9T7MK04617"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
+    providers: [provideRouter(routes),
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        provideMessaging(() => getMessaging()),
+        provideStorage(() => getStorage())
+    ]
 };
