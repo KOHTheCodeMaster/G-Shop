7119316a7f338e515cd5e88a19a2f866e85c189c
Harshit Gupta <kohthecodemaster@gmail.com>
2024-07-17 10:38:30 +0530

    #16 - Phase 1 - Skeleton Design - Using BehaviorSubject for dynamically handling login state of loggedInUser$
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,51 +1,30 @@
 import {Injectable} from '@angular/core';
 import {LoginService} from "./login.service";
 import {User} from "../interface/User";
-import {Observable, Observer} from "rxjs";
+import {BehaviorSubject, Observable, Observer} from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
 
-    loggedInUser$: Observable<User>;
+    loggedInUser$: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
+    // loggedInUser$: Observable<User | null> = this.loggedInUserSubject.asObservable();
 
     constructor(private loginService: LoginService) {
-        this.loggedInUser$ = this.initializeLoggedInUserObservable();
-    }
-
-    initializeLoggedInUserObservable(): Observable<User> {
-
-        let observer: Observer<User> = {
-            next: user => console.log('User: ' + user),
-            error: err => console.log('err: ' + err),
-            complete: () => console.log('Completed.')
-        };
-
-        let sequenceFunction = (obs: Observer<User>) => {
-            let adminUser: User = {
-                id: 0,
-                username: 'Admin',
-                email: 'admin@gmail.com',
-                password: 'admin',
-                admin: true,
-            };
-
-            obs.next(adminUser);
-            obs.complete();
-        };
-
-        let observable: Observable<User> = new Observable<User>(sequenceFunction);
-        return observable;
-
     }
 
     login(strInputEmail: string, strInputPassword: string) {
-        let loginUser = this.loginService.loginUser(strInputEmail, strInputPassword);
+
+        const user: User | null = this.loginService.loginUser(strInputEmail, strInputPassword);
+
+        if (user) this.loggedInUser$.next(user);
+        else console.error('Invalid login credentials'); // ToDo: Handle invalid login
+
     }
 
     logout() {
-        // this.loggedInUser$ = null;
+        this.loggedInUser$.next(null);
     }
 
 }
