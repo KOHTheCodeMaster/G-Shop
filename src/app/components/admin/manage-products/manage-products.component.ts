05a8fc22b1a1e3b80241fe910439cdb000ad5e9b
Harshit Gupta <kohthecodemaster@gmail.com>
2024-08-08 06:47:11 +0530

    #33 - Display Product List in Manage Products Component
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- src/app/components/admin/manage-products/manage-products.component.ts
+++ src/app/components/admin/manage-products/manage-products.component.ts
@@ -1,13 +1,23 @@
 import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {Observable} from "rxjs";
+import {Product} from "../../../interface/Product";
+import {ProductService} from "../../../service/product.service";
+import {AsyncPipe, CurrencyPipe, NgFor, NgIf} from "@angular/common";
 
 @Component({
     selector: 'app-manage-products',
     standalone: true,
-    imports: [RouterLink],
+    imports: [RouterLink, NgIf, NgFor, CurrencyPipe, AsyncPipe],
     templateUrl: './manage-products.component.html',
     styleUrl: './manage-products.component.css'
 })
 export class ManageProductsComponent {
+
+    products$: Observable<Product[]> = new Observable<Product[]>();
+
+    constructor(productService: ProductService) {
+        this.products$ = productService.getProducts();
+    }
 
 }
