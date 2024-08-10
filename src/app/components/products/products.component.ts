36160ff1bcb8c57f77ed37aaadbbac4f5dc88c15
Harshit Gupta <kohthecodemaster@gmail.com>
2024-08-10 20:20:59 +0530

    #40 - Added Categories Sidebar in Products Component
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -3,22 +3,26 @@ import {ProductService} from "../../service/product.service";
 import {Observable} from "rxjs";
 import {Product} from "../../interface/Product";
 import {AsyncPipe, CurrencyPipe, NgFor} from "@angular/common";
+import {CategoryService} from "../../service/category.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-products',
     standalone: true,
-    imports: [NgFor, AsyncPipe, CurrencyPipe],
+    imports: [NgFor, AsyncPipe, CurrencyPipe, RouterLink],
     templateUrl: './products.component.html',
     styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
     products$: Observable<Product[]>;
+    categories: string[];
 
-    constructor(productService: ProductService) {
+    constructor(productService: ProductService, categoryService: CategoryService) {
         console.log('L0G - [products.component] - constructor invoked.');
 
         this.products$ = new Observable<Product[]>();
+        this.categories = categoryService.getCategoryList();
 
         //  Wait for 1 second to load the products from json file and then initialize the products$ observable
         setTimeout(() => this.products$ = productService.getAllProducts(), 1000);
