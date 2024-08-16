0861780698838e30fbef91223eeb96f907f904db
Harshit Gupta <kohthecodemaster@gmail.com>
2024-08-16 12:21:31 +0530

    #63 - Fixed View Order Component to display correct order details
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- src/app/components/view-order/view-order.component.ts
+++ src/app/components/view-order/view-order.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {CurrencyPipe, DatePipe, NgFor, NgIf} from "@angular/common";
 import {Order} from "../../interface/Order";
 import {ActivatedRoute} from "@angular/router";
+import {AuthService} from "../../service/auth.service";
 
 @Component({
     selector: 'app-view-order',
@@ -12,16 +13,18 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class ViewOrderComponent {
 
-    order: Order | null;
+    order: Order | null = null;
 
-    constructor(route: ActivatedRoute) {
+    constructor(route: ActivatedRoute, authService: AuthService) {
 
         //  Get order id from URL Param
         let orderId = route.snapshot.params['orderId'];
 
-        //  Find order by id from orders array if exists, else set order to null
-        let orders: Order[] = JSON.parse(localStorage.getItem('orders') || '[]');
-        this.order = orders.find(order => order.id == orderId) || null;
+        //  Get all orders from local storage
+        let allOrders: { [key: number]: Order[] } = JSON.parse(localStorage.getItem('orders') || '{}');
+
+        //  Initialize order for the current user by order id if it exists, otherwise set order to null
+        this.order = allOrders[authService.getCurrentUserId()]?.find(order => order.id == orderId) || null;
 
     }
 
